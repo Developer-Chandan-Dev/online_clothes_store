f106871efd373ab40e2483787ef508ef5b23cea3
Developer-Chandan-Dev <chandan.dev.pro@gmail.com>
2025-08-25 16:26:34 +0530

    Product page error fixed

--- frontend/src/pages/Product.jsx
+++ frontend/src/pages/Product.jsx
@@ -1,7 +1,7 @@
 import { useContext, useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { ShopContext } from "../context/ShopContext";
-import RelatedProducts from "../components/RelatedPr7oducts";
+import RelatedProducts from "../components/RelatedProducts";
 import Review from "../components/Review";
 import { FavsContext } from "../context/FavsContext";
 import { toast } from "react-toastify";
